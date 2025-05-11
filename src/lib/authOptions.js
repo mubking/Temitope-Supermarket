50f9e278559559292a23536e3e575dd9a3c59a79
Mubking <adeshinamubark6@gmail.com>
2025-05-11 18:10:02 +0100

    second  push

---
+++ src/lib/authOptions.js
@@ -0,0 +1,34 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+import User from '../models/User';
+import dbConnect from './mongodb';
+import bcrypt from 'bcryptjs';
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user) throw new Error('No user found');
+
+        const isValid = await bcrypt.compare(credentials.password, user.password);
+        if (!isValid) throw new Error('Invalid password');
+
+        return { id: user._id, name: user.name, email: user.email };
+      }
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
