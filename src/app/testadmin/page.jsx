e6805ccae5a9d247f03fd8278d778b7a31c738d7
Mubaraq <adeshinamubarak6@gmail.com>
2025-06-14 00:17:21 +0100

    � Temp fix: renamed /admin route to /testadmin for production access

---
+++ src/app/testadmin/page.jsx
@@ -0,0 +1,14 @@
+import AdminDashboard from '@/components/AdminDashboard';
+import React from 'react';
+
+export const dynamic = "force-dynamic";
+
+function AdminPage() {
+  return (
+    <div>
+      <AdminDashboard />
+    </div>
+  );
+}
+
+export default AdminPage;
