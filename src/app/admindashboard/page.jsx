9bcdf6f85ed9753afc08c457a4941c482026b5b5
Mubaraq <adeshinamubarak6@gmail.com>
2025-06-26 15:41:30 +0100

    admin page

---
+++ src/app/admindashboard/page.jsx
@@ -0,0 +1,15 @@
+import AdminDashboard from '@/components/AdminDashboard';
+import React from 'react';
+
+export const dynamic = "force-dynamic";
+
+
+function AdminPage() {
+  return (
+    <div>
+      <AdminDashboard />
+    </div>
+  );
+}
+
+export default AdminPage;
