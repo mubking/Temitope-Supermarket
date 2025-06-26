2d1246000c6d4de966ae646d92f0062ad4be9323
Mubaraq <adeshinamubarak6@gmail.com>
2025-06-26 08:58:43 +0100

    admin page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -2,8 +2,6 @@ import AdminDashboard from '@/components/AdminDashboard';
 import React from 'react';
 
 export const dynamic = "force-dynamic";
-console.log("📡 [AdminDashboard] session", session);
-console.log("📡 [AdminDashboard] status", status);
 
 
 function AdminPage() {
