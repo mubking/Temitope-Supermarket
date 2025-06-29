01c3a7ff1be73ce6acd3b395995751753948be2a
Mubaraq <adeshinamubarak6@gmail.com>
2025-06-29 18:08:00 +0100

    next auth correction

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,13 @@ import NextAuth from "next-auth";
 import { getAuthOptions } from "@/lib/authOptions";
 
 const handler = async (...args) => {
-  const options = await getAuthOptions();
-  return NextAuth(...args, options);
+  try {
+    const options = await getAuthOptions();
+    return NextAuth(...args, options);
+  } catch (err) {
+    console.error("❌ NextAuth production error:", err);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 };
 
 export { handler as GET, handler as POST };
