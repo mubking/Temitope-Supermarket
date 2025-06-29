0a5dafa5dd7972a86c5ce349f25a844d16192b6d
Mubaraq <adeshinamubarak6@gmail.com>
2025-06-29 18:53:08 +0100

    correction

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth";
 import { getAuthOptions } from "@/lib/authOptions";
 
-const handler = async (req) => {
-  const options = await getAuthOptions();
-  return NextAuth(req, options);
-};
+const authOptions = await getAuthOptions();
 
-export const GET = handler;
-export const POST = handler;
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
