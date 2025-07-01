7fb216a567dd83e6e6845248aa170164b3163ffc
Mubaraq <adeshinamubarak6@gmail.com>
2025-07-02 00:57:57 +0100

    � Fix import path for authoptions

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,9 @@
 import NextAuth from "next-auth";
 import { getAuthOptions } from "@/lib/authoptions";
 
-const handler = async (...args) => {
+const handler = async (req, res) => {
   const authOptions = await getAuthOptions();
-  return NextAuth(authOptions)(...args);
+  return NextAuth(authOptions)(req, res);
 };
 
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST };
